ed98a33092fdca946199a8f70b05250a1fc1e103
Sergio <23checo06@gmail.com>
2025-10-02 10:16:32 -0600

    Initial Commit

---
+++ AppPokeApi/AppPokeApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPokeApiApp.swift
+//  AppPokeApi
+//
+//  Created by Sergio Rodríguez Pérez on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AppPokeApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
